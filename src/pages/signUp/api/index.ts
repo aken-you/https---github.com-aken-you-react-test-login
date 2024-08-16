431259df65220186aa2d852a84c40e53cd0f5b62
aken-you <kinsk2839@naver.com>
2024-08-16 22:05:13 +0900

    refactor: 이메일 회원가입 api 요청 로직 수정

--- src/pages/signUp/api/index.ts
+++ src/pages/signUp/api/index.ts
@@ -2,13 +2,9 @@ import type { Form } from "../types";
 
 // "이메일" 회원 가입
 // ? passwordConfirm도 요청 보내야하는지 확인
-export const signUpFirstStep = async ({
-  email,
-  password,
-}: // passwordConfirm,
-Form) => {
+export const signUpFirstStep = async ({ email, password }: Form) => {
   try {
-    const response = await fetch("http://localhost:8080/auth/email", {
+    const response = await fetch("http://localhost:8080/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,7 +12,6 @@ Form) => {
       body: JSON.stringify({
         email,
         password,
-        // passwordConfirm,
       }),
     });
 
